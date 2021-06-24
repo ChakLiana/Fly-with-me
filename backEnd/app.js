4f685f4b92637dfd54e1ae84308d4323fdfb29b0
liana <lianova1209@bk.ru>
2021-06-24 12:31:53 +0300

    second update

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const logger = require('morgan');
 const app = express()
 const PORT = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(logger('dev'));
 
 app.listen(PORT, () => {
   console.log('Server has been started on port ', PORT)
