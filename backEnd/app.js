eaecd0d1cb6c5fcb7375c8e759a8846c01bbc732
Roman <makatsariya_roma@mail.ru>
2021-06-24 15:12:17 +0300

    config&connect db

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan');
+const dbConnect = require('./src/db/dbConect');
 const app = express()
 const PORT = 8080
 
@@ -8,6 +9,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(logger('dev'));
+
+dbConnect()
 
 app.listen(PORT, () => {
   console.log('Server has been started on port ', PORT)
