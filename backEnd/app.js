925b24011c95ca7a2cb484a86ec3b112ffd09807
Sergey Kudryashov <kudray@yandex.ru>
2021-06-25 14:22:43 +0300

    back for images

--- backEnd/app.js
+++ backEnd/app.js
@@ -3,12 +3,39 @@ const cors = require('cors')
 const logger = require('morgan');
 const dbConnect = require('./src/db/dbConect');
 const app = express()
-const PORT = 8080
+const PORT = 3007
+// подключаем multer для поддержки загрузки картинок
+const multer = require('multer') 
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(logger('dev'));
+
+// multer section 
+// задаём параметры хранилища
+const store = multer.diskStorage({
+    destination(req, file, cb) {
+          cb(null, './src/public/images'); // папка файлов на сервере
+        },
+    filename(req, file, cb) {
+          cb(null, Date.now() + '_' + file.originalname); // Каждому файлу поставим дату 
+        },
+});
+// прописываем функции для приёма файлов
+const upload = multer({ storage: store }).single('file'); // загрузка одного файла
+const uploadMany = multer({ storage: store }).array('files'); // загрузка массива файлов
+
+// прописываем "ручки" 
+app.put('/image', upload, (req, res, next) => {
+   req.body.file // файл 
+});
+app.put('/image', uploadMany, (req, res, next) => {
+   req.body.files // массив файлов
+});
+
+
 
 dbConnect()
 
