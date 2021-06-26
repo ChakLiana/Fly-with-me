91837760e58609aba594b59bf0b3a38d07c9a7b2
Sergey Kudryashov <kudray@yandex.ru>
2021-06-26 19:20:35 +0300

    June 26 end of the day

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,56 +1,56 @@
-const express = require('express');
-const cors = require('cors');
-const logger = require('morgan');
-const dbConnect = require('./src/db/dbConect');
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const dbConnect = require("./src/db/dbConect");
+const path = require("path");
+// import files upload library
+const fileUpload = require("express-fileupload");
 
-const app = express()
-const PORT = 8080
+const app = express();
+const PORT = 3007;
 // подключаем multer для поддержки загрузки картинок
-const multer = require('multer') 
-
-
+const multer = require("multer");
 
 // Импорт ручек
-const iventRouter = require('./src/routers/iventRouter');
+const iventRouter = require("./src/routers/iventRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(logger('dev'));
-
+app.use(logger("dev"));
 
 // Подключение ручек
-app.use('/ivent', iventRouter);
+app.use("/ivent", iventRouter);
 
-// multer section 
-// задаём параметры хранилища
-const store = multer.diskStorage({
-    destination(req, file, cb) {
-          cb(null, './src/public/images'); // папка файлов на сервере
-        },
-    filename(req, file, cb) {
-          cb(null, Date.now() + '_' + file.originalname); // Каждому файлу поставим дату 
-        },
+
+// start of EXPRESS file uploader section
+app.use(fileUpload());
+
+app.post("/upload", function (req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.file;
+
+  uploadPath = __dirname + "/src/public/images/" + sampleFile.name;
+  console.log("upload PATH _____", uploadPath);
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send("File uploaded!");
+  });
 });
-// прописываем функции для приёма файлов
-const upload = multer({ storage: store }).single('file'); // загрузка одного файла
-const uploadMany = multer({ storage: store }).array('files'); // загрузка массива файлов
 
-// прописываем "ручки" 
-app.put('/image', upload, (req, res, next) => {
-   req.body.file // файл 
-   res.sendStatus(200)
-});
-app.put('/image', uploadMany, (req, res, next) => {
-   req.body.files // массив файлов
-   res.sendStatus(200)
-});
- 
-
-
-dbConnect()
-
+// END OF  EXPRESS file uploader section
+dbConnect();
 
 app.listen(PORT, () => {
-  console.log('Server has been started on port ', PORT);
+  console.log("Server has been started on port ", PORT);
 });
