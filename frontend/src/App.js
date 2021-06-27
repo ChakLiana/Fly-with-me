3c11a48ff5663f8073b9cfe34137ea9a4ad67525
VoronovY <voronov.yuri87@gmail.com>
2021-06-27 15:16:47 +0300

    edit form user/tandem + models db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,51 +2,51 @@ import YandexMap from "./components/YandexMap/YandexMap";
 import ProfileUser from "./Profiles/ProfileUser.jsx";
 import NavBarCompot from "./components/NavBarCompot/NavBarCompot";
 import Main from "./components/Main/Main";
-import Login from './components/auth/Auth/LoginForm'
-import Register from './components/auth/Auth/SignupForm'
-import { Route,Switch } from "react-router-dom";
-import React, { useState, useEffect } from 'react';
-import AUTH from './utils/AUTH'
+import Login from "./components/auth/Auth/LoginForm";
+import Register from "./components/auth/Auth/SignupForm";
+import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import AUTH from "./utils/AUTH";
+import RoleSwitches from "./Profiles/RoleSwitches";
 
-//      
-
+//
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
-  
-  useEffect(() => {
-    AUTH.getUser().then(response => {
-        // console.log(response.data);
-        if (!!response.data.user) {
-          setLoggedIn(true);
-          setUser(response.data.user);
-        } else {
-          setLoggedIn(false);
-          setUser(null);
-        }
-      });
 
-      return () => {
+  useEffect(() => {
+    AUTH.getUser().then((response) => {
+      // console.log(response.data);
+      if (!!response.data.user) {
+        setLoggedIn(true);
+        setUser(response.data.user);
+      } else {
         setLoggedIn(false);
         setUser(null);
-      };
+      }
+    });
+
+    return () => {
+      setLoggedIn(false);
+      setUser(null);
+    };
   }, []);
 
-	const logout = (event) => {
+  const logout = (event) => {
     event.preventDefault();
-    
-		AUTH.logout().then(response => {
-			// console.log(response.data);
-			if (response.status === 200) {
-				setLoggedIn(false);
-        setUser(null);
-			}
-		});
-	};
 
-	const login = (username, password) => {
-		AUTH.login(username, password).then(response => {
+    AUTH.logout().then((response) => {
+      // console.log(response.data);
+      if (response.status === 200) {
+        setLoggedIn(false);
+        setUser(null);
+      }
+    });
+  };
+
+  const login = (username, password) => {
+    AUTH.login(username, password).then((response) => {
       console.log(response.data);
       if (response.status === 200) {
         // update the state
@@ -54,25 +54,26 @@ function App() {
         setUser(response.data.user);
       }
     });
-	};
+  };
 
   return (
     <div className="App">
-     < Switch>
-      { loggedIn && (
-        <div>
-          <NavBarCompot  logout={logout} />
-        </div>
-       
-      )}
-      { !loggedIn && (
-         <div className="auth-wrapper" style={{paddingTop:40}}>
-         <NavBarCompot />
-        <Route exact path="/" component={() => <Login login={login}/>} />
-       <Route exact path="/main" component={Main}/>
-        <Route exact path="/signup" component={Register} /> 
-      </div>
-      )}
+      <Switch>
+        {loggedIn && (
+          <div>
+            <NavBarCompot logout={logout} />
+          </div>
+        )}
+        {!loggedIn && (
+          <div className="auth-wrapper" style={{ paddingTop: 40 }}>
+            <NavBarCompot />
+            <Route exact path="/" component={() => <Login login={login} />} />
+            <Route exact path="/main" component={Main} />
+            <Route exact path="/signup" component={Register} />
+
+            <Route exact path="/profile" component={RoleSwitches} />
+          </div>
+        )}
       </Switch>
     </div>
   );
--- frontend/src/Profiles/ProfileUser.jsx
+++ frontend/src/Profiles/ProfileUser.jsx
@@ -6,6 +6,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 // import { useDispatch, useSelector } from "react-redux";
 // import { editSquareThunk } from "../../redux/actions/tictac";
+import { Grid, Paper, ButtonBase } from "@material-ui/core";
 
 const useStyles = makeStyles({
   span: {
@@ -38,71 +39,84 @@ export default function ProfileUser() {
   // const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Личный кабинет пользователя
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Имя:
-        </Typography>
-        <Typography
-          className={classes.pos}
-          component="span"
-          color="textPrimary"
-        >
-          Василий
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Ник:
-        </Typography>
-        <Typography
-          className={classes.pos}
-          component="span"
-          color="textPrimary"
-        >
-          Василий
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Возраст:
-        </Typography>
-        <Typography
-          className={classes.pos}
-          component="span"
-          color="textPrimary"
-        >
-          Василий
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Вес:
-        </Typography>
-        <Typography
-          className={classes.pos}
-          component="span"
-          color="textPrimary"
-        >
-          Василий
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Контакт:
-        </Typography>
-        <Typography
-          className={classes.pos}
-          component="span"
-          color="textPrimary"
-        >
-          Василий
-        </Typography>
-        <Typography variant="body2" component="p">
-          <br />
-          Для редактирования информации о себе нажмите кнопку:
-          <br />
-          {'"ОТРЕДАКТИРОВАТЬ ДАННЫЕ"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="big">Отредактировать данные</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Grid justify-center>
+        <Grid item>
+          <Typography variant="h5" component="h2">
+            Личный кабинет пользователя
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item>
+          <Typography> </Typography>
+        </Grid>
+        <Grid item>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography className={classes.pos} color="textSecondary">
+                Имя:
+              </Typography>
+              <Typography
+                className={classes.pos}
+                component="span"
+                color="textPrimary"
+              >
+                Василий
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Ник:
+              </Typography>
+              <Typography
+                className={classes.pos}
+                component="span"
+                color="textPrimary"
+              >
+                Василий
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Возраст:
+              </Typography>
+              <Typography
+                className={classes.pos}
+                component="span"
+                color="textPrimary"
+              >
+                Василий
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Вес:
+              </Typography>
+              <Typography
+                className={classes.pos}
+                component="span"
+                color="textPrimary"
+              >
+                Василий
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Контакт:
+              </Typography>
+              <Typography
+                className={classes.pos}
+                component="span"
+                color="textPrimary"
+              >
+                Василий
+              </Typography>
+              <Typography variant="body2" component="p">
+                <br />
+                Для редактирования информации о себе нажмите кнопку:
+                <br />
+                {'"ОТРЕДАКТИРОВАТЬ ДАННЫЕ"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="big">Отредактировать данные</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
