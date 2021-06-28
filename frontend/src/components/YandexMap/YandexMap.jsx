db7f543cc1231acf5deed1b02cd6d1b1a24e1890
liana <lianova1209@bk.ru>
2021-06-28 17:11:03 +0300

    ?

--- frontend/src/components/YandexMap/YandexMap.jsx
+++ frontend/src/components/YandexMap/YandexMap.jsx
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { YMaps, Map, Placemark, Clusterer, TypeSelector, SearchControl, ZoomControl } from "react-yandex-maps";
+import {
+  YMaps,
+  Map,
+  Placemark,
+  Clusterer,
+  TypeSelector,
+  SearchControl,
+  ZoomControl,
+} from "react-yandex-maps";
 import { iventInitFromBack } from "../../redux/actions/iventActions";
 import { currentCoordsGet } from "../../redux/actions/currentCoordsActions";
 
@@ -26,7 +34,7 @@ function YandexMap() {
 
   window.bla = (a) => {
     console.log(a);
-  }
+  };
 
   return (
     <YMaps query={{ lang: "ru_RU", ns: "use-load-option", apikey: key }}>
@@ -44,7 +52,7 @@ function YandexMap() {
           "control.FullscreenControl",
           "geocode",
         ]}
-      //   className={styles.map}
+        //   className={styles.map}
       >
         <TypeSelector options={{ float: "right" }} />
         <SearchControl options={{ float: "left" }} />
@@ -52,26 +60,26 @@ function YandexMap() {
         {/* <Clusterer options={{ groupByCoordinates: false }}> */}
         {curentCoords.length ? <Placemark geometry={curentCoords} /> : null}
 
-        {allIvents.length && currentUser.role === 'passenger'
+        {allIvents.length && currentUser?.role === "passenger"
           ? allIvents.map((elem) => (
-            < Placemark
-              key={elem._id}
-              geometry={elem.coords}
-              modules={["geoObject.addon.balloon"]}
-              properties={{
-                balloonContentHeader: `<h5>Здесь летает ${elem.creator.nickName}</h5>`,
-                balloonContentBody: `
+              <Placemark
+                key={elem._id}
+                geometry={elem.coords}
+                modules={["geoObject.addon.balloon"]}
+                properties={{
+                  balloonContentHeader: `<h5>Здесь летает ${elem.creator.nickName}</h5>`,
+                  balloonContentBody: `
                 <p> Когда: ${elem.dateOfEvent}</p>
                 <p>Сколько стоит: ${elem.price} р.</p>
                 <p>Какие требования к пассажиру: ${elem.stopList}</p>
                 <p> Координаты старта: ${elem.coords[0]}, ${elem.coords[1]}</p>`,
-                balloonContentFooter: `<button  class ='btn btn-info' onclick="window.bla(${(elem.coords[0])})" >Полетать</button>`,
-              }}
-
-            />
-          )) : allIvents.length && currentUser.role === 'tandem'
-            ? allIvents.map((elem) => (
-              < Placemark
+                  balloonContentFooter: `<button  class ='btn btn-info' onclick="window.bla(${elem.coords[0]})" >Полетать</button>`,
+                }}
+              />
+            ))
+          : allIvents.length && currentUser?.role === "tandem"
+          ? allIvents.map((elem) => (
+              <Placemark
                 key={elem._id}
                 geometry={elem.coords}
                 modules={["geoObject.addon.balloon"]}
@@ -83,13 +91,12 @@ function YandexMap() {
                 <p>Какие требования к пассажиру: ${elem.stopList}</p>
                 <p> Координаты старта: ${elem.coords[0]}, ${elem.coords[1]}</p>`,
                 }}
-
               />
-            )) : null}
+            ))
+          : null}
         {/* </Clusterer> */}
       </Map>
-
-    </YMaps >
+    </YMaps>
   );
 }
 
