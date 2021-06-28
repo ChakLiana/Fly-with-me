0c54be9538775627912c39e3838981df4f2b152a
Sergey Kudryashov <kudray@yandex.ru>
2021-06-28 11:47:52 +0300

    June 28 11-47 registration

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,15 @@
 import { applyMiddleware, createStore } from "redux";
-import rootReduser from './redusers/rootReduser';
-import initialState from './initialState';
+import rootReduser from "./redusers/rootReduser";
+import initialState from "./initialState";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 
+const temp = (state, action) => {
+  return state;
+};
 const store = createStore(
   rootReduser,
+  // temp,
   initialState,
   composeWithDevTools(applyMiddleware(thunkMiddleware))
 );
--- frontend/src/redux/types/userTypes.js
+++ frontend/src/redux/types/userTypes.js
@@ -1,2 +1,4 @@
 export const SET_USER = 'SET_USER'
 export const DELETE_USER = 'DELETE_USER'
+export const TOKEN_TO_STORE = 'TOKEN_TO_STORE'
+
